8e0bc2cf9e864bf338624efe65b1cd9730ab8deb
User <user@example.com>
2026-03-16 12:56:12 +0300

    2. Добавлены импорты и хранилище для products

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, Query
+from pydantic import BaseModel, Field
+from datetime import datetime
+from typing import Optional
 
 app = FastAPI()
+
+# In-memory хранилища
+products_db: dict[int, dict] = {}
+products_counter = 0
