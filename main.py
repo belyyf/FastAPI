086921e2a1fed1ed735ecfda7e75643877487d23
User <user@example.com>
2026-03-16 12:56:29 +0300

    3. Добавлены Pydantic модели для Products

--- main.py
+++ main.py
@@ -8,3 +8,23 @@ app = FastAPI()
 # In-memory хранилища
 products_db: dict[int, dict] = {}
 products_counter = 0
+
+
+# Products models
+class ProductCreate(BaseModel):
+    name: str = Field(..., min_length=2, max_length=80)
+    price: int = Field(..., ge=0)
+    in_stock: bool = True
+
+
+class ProductUpdate(BaseModel):
+    name: str = Field(..., min_length=2, max_length=80)
+    price: int = Field(..., ge=0)
+    in_stock: bool
+
+
+class ProductResponse(BaseModel):
+    id: int
+    name: str
+    price: int
+    in_stock: bool
